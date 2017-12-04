6ddaf659d4a864af1a1778b02557ae5ad0216fad
Nico Kreipke <git@nkreipke.de>
2017-12-04 12:38:41 +0100

    Skript entfernt

--- DeleteSymbolPackages.ps1
+++
@@ -1,6 +0,0 @@
-﻿# Wir löschen die NuGet-Pakete mit integrierten Symbolen, um sie nicht
-# automatisch zu veröffentlichen
-
-$ErrorActionPreference = "Stop"
-
-Get-ChildItem "*.symbols.nupkg" -Recurse | Remove-Item
