968b95b3a81e3705fb6bb4e18f7cc7423ae19260
Nico <git@nkreipke.de>
2020-01-31 10:35:39 +0100

    PAP 2014-2018 als alt markiert (werden nicht mehr automatisch erstellt, neuer Switch BuildOld)

--- Build.ps1
+++ Build.ps1
@@ -4,6 +4,7 @@
 Param(
     [switch]$Test,
     [switch]$Pack,
+    [switch]$BuildOld,
     [switch]$RebuildExisting,
     [String]$Version="0.0.1",
     [String]$PackOutput="dist"
@@ -42,10 +43,20 @@ if (!(Test-Path $generatedDir)) {
     New-Item -ItemType Directory -Path $generatedDir
 }
 
-foreach ($jahr in Get-ChildItem -Directory $targetDir) {
-    $jahrKurz = $jahr.Name.Substring(2, 2)
+foreach ($jahrPath in Get-ChildItem -Directory $targetDir) {
+    if ($jahrPath.Name.EndsWith("-old")) {
+        if (!$BuildOld) {
+            continue;
+        }
+
+        $jahr = $jahrPath.Name.Substring(0, $jahrPath.Name.Length - 4)
+    } else {
+        $jahr = $jahrPath.Name
+    }
+
+    $jahrKurz = $jahr.Substring(2, 2)
     $name = "Dataline.Tax.LSt$jahr"
-    $currentDir = Join-Path -Path $targetDir -ChildPath $jahr
+    $currentDir = Join-Path -Path $targetDir -ChildPath $jahrPath
     $papPath = Join-Path -Path $currentDir -ChildPath "pap.xml"
     $outDir = Join-Path -Path $generatedDir -ChildPath $name
 
@@ -59,11 +70,11 @@ foreach ($jahr in Get-ChildItem -Directory $targetDir) {
         $testCsvs = Get-ChildItem (Join-Path -Path $currentDir -ChildPath "test-*.csv")
     
         Write-Progress -Activity $name -Status "Konvertiere PAP"
-        Convert-BmfPap -PapPath $papPath -Year $jahr.Name -OutputDirectory $outDir -Namespace $name -TestDataPaths $testCsvs -ProjectAuthor "DATALINE GmbH & Co. KG" -ProjectCopyright "$((Get-Date).Year) DATALINE GmbH & Co. KG" -ProjectDescription "BMF-PAP Lohnsteuerberechnung $jahr" -ProjectVersion $Version -ProjectTags "DATALINE", "$jahr" -Extensions "TariflicheEinkommensteuer" -Macros "yearshort", $jahrKurz
+        Convert-BmfPap -PapPath $papPath -Year $jahr -OutputDirectory $outDir -Namespace $name -TestDataPaths $testCsvs -ProjectAuthor "DATALINE GmbH & Co. KG" -ProjectCopyright "$((Get-Date).Year) DATALINE GmbH & Co. KG" -ProjectDescription "BMF-PAP Lohnsteuerberechnung $jahr" -ProjectVersion $Version -ProjectTags "DATALINE", "$jahr" -Extensions "TariflicheEinkommensteuer" -Macros "yearshort", $jahrKurz
         CheckExitCode
     }
 
-    if ($Test -and $jahr.Name -ne "2014") { # Der PAP 2014 unterstützt das Testprojekt nicht
+    if ($Test -and $jahr -ne "2014") { # Der PAP 2014 unterstützt das Testprojekt nicht
         Write-Progress -Activity $name -Status "Ausführung Testprojekt"
         dotnet test (Join-Path -Path $outDir -ChildPath "$name.Test\$name.Test.csproj")
         CheckExitCode
