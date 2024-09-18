716e968e2b55a6788441bd80a4812f4f788d0dfe
marcus.seiferth@dataline.de <marcus.seiferth@dataline.de>
2024-09-18 14:29:45 +0200

    Extension für 202412 aufgenommen wegen Änderung der Methode UPTAB24

---
+++ src/Dataline.Tax.BmfPapConverter/Resources/Extension_TariflicheEinkommensteuer_202412.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// Berechnet die tarifliche Einkommensteuer.
+/// </summary>
+/// <param name="zve">
+/// Zu versteuerndes Einkommen gem. § 32a Abs. 1 und 2 EStG €, C
+/// (2 Dezimalstellen)
+/// </param>
+/// <param name="kztab">
+/// Kennzahl fuer die Einkommensteuer-Tabellenart:
+/// 1 = Grundtabelle
+/// 2 = Splittingtabelle
+/// </param>
+/// <returns>Die tarifliche Einkommensteuer.</returns>
+public decimal TariflicheEinkommensteuer(decimal zve, int kztab)
+{
+    KZTAB = kztab;
+    X = Floor((zve / ((decimal)KZTAB)), 0);
+    UPTAB24A();
+    return ST;
+}
